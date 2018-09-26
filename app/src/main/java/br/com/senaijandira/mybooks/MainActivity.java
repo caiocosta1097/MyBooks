36be93fa35d4118270cfacd3570b9746d29d4684
CAIO DA COSTA CARMO <17259196@CFP127ALUNO.LOCAL>
2018-09-26 08:55:28 -0300

    Implementação do ListView

--- app/src/main/java/br/com/senaijandira/mybooks/MainActivity.java
+++ app/src/main/java/br/com/senaijandira/mybooks/MainActivity.java
@@ -1,22 +1,30 @@
 package br.com.senaijandira.mybooks;
 
 import android.arch.persistence.room.Room;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import br.com.senaijandira.mybooks.db.MyBooksDatabase;
 import br.com.senaijandira.mybooks.model.Livro;
 
 public class MainActivity extends AppCompatActivity {
 
-    LinearLayout listaLivros;
+    ListView listaViewLivros;
+
+    LivrosAdapter adapter;
 
     public static Livro[] livros;
 
@@ -34,23 +42,11 @@ public class MainActivity extends AppCompatActivity {
                 .allowMainThreadQueries()
                 .build();
 
-        listaLivros = findViewById(R.id.listaLivros);
+        listaViewLivros = findViewById(R.id.listViewLivros);
 
-        //Criando cadastros Fake
-        livros = new Livro[]{
-//                new Livro(1,
-//                        Utils.toByteArray(getResources(), R.drawable.pequeno_principe ),
-//                        "O pequeno principe", getString(R.string.pequeno_principe)
-//                        ),
-//                new Livro(2,
-//                        Utils.toByteArray(getResources(), R.drawable.cinquenta_tons_cinza ),
-//                        "50 Tons de cinza", getString(R.string.cinquenta_tons)
-//                ),
-//                new Livro(3,
-//                        Utils.toByteArray(getResources(), R.drawable.kotlin_android ),
-//                        "Kotlin com Android", getString(R.string.kotlin_android)
-//                )
-        };
+        adapter = new LivrosAdapter(this);
+
+        listaViewLivros.setAdapter(adapter);
 
     }
 
@@ -59,24 +55,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
+        adapter.clear();
+
         // Aqui faz um select no banco
         livros = myBooksDatabase.livroDao().selecionarTodos();
 
-        listaLivros.removeAllViews();
-
-        for(Livro l : livros){
-            criarLivro(l, listaLivros);
-        }
+        adapter.addAll(livros);
 
     }
 
-    public void deletarLivro(Livro livro, View v){
+    public void deletarLivro(Livro livro){
 
         // Remover do banco de dados
         myBooksDatabase.livroDao().deletar(livro);
 
         // Remover item da tela
-        listaLivros.removeView(v);
+        adapter.remove(livro);
 
     }
 
@@ -95,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
         imgDeleteLivro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                deletarLivro(livro, v);
+                deletarLivro(livro);
             }
         });
 
@@ -117,5 +111,54 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(this, CadastroActivity.class));
     }
 
+
+    private class LivrosAdapter extends ArrayAdapter<Livro>{
+
+        public LivrosAdapter(Context context){
+
+            super(context, 0, new ArrayList<Livro>());
+
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+            View view = convertView;
+
+            if (view == null){
+
+                view = LayoutInflater.from(getContext()).inflate(R.layout.livro_layout, parent, false);
+
+            }
+
+            final Livro livro = getItem(position);
+
+            ImageView imgLivroCapa = view.findViewById(R.id.imgLivroCapa);
+            TextView txtLivroTitulo = view.findViewById(R.id.txtLivroTitulo);
+            TextView txtLivroDescricao = view.findViewById(R.id.txtLivroDescricao);
+
+            ImageView imgDeleteLivro = view.findViewById(R.id.imgDeleteLivro);
+
+            imgDeleteLivro.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    deletarLivro(livro);
+                }
+            });
+
+            //Setando a imagem
+            imgLivroCapa.setImageBitmap( Utils.toBitmap(livro.getCapa()) );
+
+            //Setando o titulo do livro
+            txtLivroTitulo.setText(livro.getTitulo());
+
+            //Setando a descrição do livro
+            txtLivroDescricao.setText(livro.getDescricao());
+
+            return view;
+
+        }
+    }
 
 }
